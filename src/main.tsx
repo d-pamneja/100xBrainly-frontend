12ce418d1a519a2d434d0f2d1278285131eb1ca8
d-pamneja <dpamneja@gmail.com>
2024-11-28 11:40:03 +0530

    Deployment - TSCONFIG Changes

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 import axios from 'axios';
 
-axios.defaults.baseURL = "https://100x-brainly.vercel.app";
+axios.defaults.baseURL = "https://100x-brainly-backend.vercel.app";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')!).render(
